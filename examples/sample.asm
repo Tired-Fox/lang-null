5147fcd1f3f2d744b29cf9cebd696f6614a76fbe
Zachary Boehm <zboehm@menard-inc.com>
2023-09-08 17:37:54 -0500

    Add simple nasm example

---
+++ examples/sample.asm
@@ -0,0 +1,34 @@
+global start
+
+extern printf        ;from msvcrt
+extern scanf         ;from msvcrt
+extern ExitProcess   ;from kernel32
+
+section .bss
+name:   resb 100
+
+section .data
+prompt: db 'Enter your name: ',0
+frmt:   db '%s',0
+greet:  db 'Hello, %s!',0ah,0
+
+section .text
+start:
+        sub     rsp,40
+
+        mov     rcx,prompt
+        call    printf
+
+        mov     rdx,name
+        mov     rcx,frmt
+        call    scanf
+
+        mov     rdx,name
+        mov     rcx,greet
+        call    printf
+
+        xor     ecx,ecx
+        call    ExitProcess
+
+;nasm -f win64 myprog.asm
+;golink /console myprog.obj msvcrt.dll kernel32.dll
